5555425e06ad23a0ddf676da844904b75b70e6e0
Ihtizaz Nasir <ihtizaznasir342@gmail.com>
2025-09-06 09:42:59 +0500

    Add files via upload

---
+++ calculator/calculator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//this is my first time of coding //
+
+int main() {
+ int a, b;
+ printf("enter a");
+ scanf("%d", &a);
+ 
+ printf("enter b");
+ scanf("%d", &b);
+ 
+ printf("sum is : %d", a+b);
+ printf("sub is :%d", a-b);
+ printf("multiplication is :%d", a*b);
+ printf("division is :%d", a/b);
+	return 0;
+}
